02cc44cd77c43d51dc0bcdaa30d69631478b31bb
taisei.inada <taisei92356ooo@gmail.com>
2019-10-12 11:35:33 +0900

    Add NewServlet.java

--- src/models/Task.java
+++ src/models/Task.java
@@ -45,6 +45,14 @@ public class Task {
         this.id = id;
     }
 
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
     public String getContent() {
         return content;
     }
